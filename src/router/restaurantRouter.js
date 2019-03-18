8799af811d483ad7455bbc80a468934983e39aae
Steven Shidi Zhou <steven.zhou930605@gmail.com>
2019-03-18 20:22:59 +0100

    add some comments

--- src/router/restaurantRouter.js
+++ src/router/restaurantRouter.js
@@ -2,10 +2,17 @@ const express = require('express');
 const controllers = require('../controllers/restaurantControllers');
 const router = express.Router();
 
-// /api/restaurant
-
+/* /api/restaurant
+    GET:  Read all restaurants
+    POST: Create one restaurant 
+*/
 router.route('/').get(controllers.getMany).post(controllers.createOne);
 
-// /api/restaurant/:id
+/* /api/restaurant/:id
+    GET: Read one restaurant
+    PUT: Update one restaurant
+    DELETE: delete one restaurant
+    
+*/
 router.route('/:id').get(controllers.getOne).put(controllers.updateOne).delete(controllers.removeOne);
 module.exports = router;
