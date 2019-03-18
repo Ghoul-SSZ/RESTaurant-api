ae3fe7dd64edcc58e736ddc83cbd079dd5154e9c
Steven Shidi Zhou <steven.zhou930605@gmail.com>
2019-03-18 19:24:27 +0100

    added swagger api documentation

--- src/app.js
+++ src/app.js
@@ -4,12 +4,15 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const restaurantRouter = require('./router/restaurantRouter');
 const config = require('./config/config');
-
 const mongoose = require('mongoose');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
 
 const app = express();
+//connecting to mongodb
 mongoose.connect('mongodb://localhost:27017/viaplay', { useNewUrlParser: true });
 
+//middleware
 app.use(cors());
 app.use(
 	bodyParser.urlencoded({
@@ -19,8 +22,12 @@ app.use(
 app.use(bodyParser.json());
 app.use(morgan('dev'));
 
+//swagger api
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+//routing
 app.use('/api/restaurant', restaurantRouter);
 
+//listen on port 3000 right now
 app.listen(config.port, () => {
 	console.log(`server is running in port: ${config.port}`);
 });
